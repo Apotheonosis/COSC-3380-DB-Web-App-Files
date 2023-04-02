9da85af03f94d7ca0eb98b16eb3da89885ef503c
Apotheonosis <124922701+Apotheonosis@users.noreply.github.com>
2023-04-02 17:01:25 -0500

    Create connectToTheDB.php

---
+++ connectToTheDB.php
@@ -0,0 +1,8 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "clinic_db";
+
+    $conn = new mysqli($servername, $username, $password, $dbname, 3306) or die("not connected");
+?>
